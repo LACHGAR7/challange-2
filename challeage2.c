78888187d673d7fecbbb515fbfd30679d43ad4b9
LACHGAR7 <112889457+LACHGAR7@users.noreply.github.com>
2022-09-12 08:51:52 +0100

    chalenge

---
+++ challeage2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   //Paire ou Impaire
+
+    int nombre;
+    printf("ectere un nombre :");
+    scanf("%d",&nombre);
+
+       if(nombre%2== 0)
+       {
+        printf ("le nombre paire");
+       }else printf("le nombre inparie");
+
+
+
+}
+
